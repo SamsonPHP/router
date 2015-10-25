bf25c26f80dadf3196983038a50a271c46d70de5
Vitaly Iegorov <vitalyiegorov@gmail.com>
2015-10-25 20:53:50 +0200

    Added route parameters filters Added comments Refactored

--- src/Core.php
+++ src/Core.php
@@ -17,23 +17,49 @@ class Core
     /** @var array Collection of all application routes */
     protected $routes = array();
 
+    /**
+     * Create router logic function.
+     * This method is recursive
+     * @param array|string $dataPointer Collection of routes or route identifier
+     * @param string $path Current route tree path
+     * @param string $code Final result
+     * @param int $level Recursion level
+     * @return string Router logic function
+     */
     protected function generateIf(&$dataPointer, $path, &$code = '', $level = 1)
     {
+        /** @var bool $conditionStarted Flag for creating conditions */
         $conditionStarted = false;
+
+        // Count left spacing to make code looks better
         $tabs = implode('', array_fill(0, $level, ' '));
         foreach ($dataPointer as $placeholder => $data) {
+            // Concatenate path
             $newPath = $path . '/' . $placeholder;
+
+            // Add route description as a comment
             $code .= $tabs . '// ' . $newPath . "\n";
+
+            // Count indexes
             $stIndex = strlen($path) + 1;
             $length = strlen($placeholder);
 
-            // This is a route variable
-            if (strpos($placeholder, '{') !== false) {
-                $code .= $tabs . 'if (preg_match("/(?<' . $placeholder . '>[0-9a-z_]+)/i", substr($path, ' . $stIndex . ',  strpos($path, "/", ' . $stIndex . ') ? strlen($path) - strpos($path, "/", ' . $stIndex . ') : 0), $matches)) {' . "\n";
+            // Check if placeholder is a route variable
+            if (preg_match('/{(?<name>[^}:]+)(\t*:\t*(?<filter>[^}]+))?}/i', $placeholder, $matches)) {
+                // Define parameter filter or use generic
+                $filter = isset($matches['filter']) ? $matches['filter'] : '[0-9a-z_]+';
+
+                // Generate parameter route parsing, logic is that parameter can have any length so we
+                // limit it either by closest brace(}) to the right or to the end of the string
+                $code .= $tabs . 'if (preg_match("/(?<' . $matches['name'] . '>'.$filter.'+)/i", substr($path, ' . $stIndex . ',  strpos($path, "/", ' . $stIndex . ') ? strlen($path) - strpos($path, "/", ' . $stIndex . ') : 0), $matches)) {' . "\n";
+
                 // When we have route parameter we do not split logic tree as different parameters can match
                 $conditionStarted = false;
             } else { // Generate route placeholder comparison
                 $code .= $tabs . ($conditionStarted ? 'else ' : '') . 'if (substr($path, ' . $stIndex . ', ' . $length . ') === "' . $placeholder . '" ) {' . "\n";
+
+                // Flag that condition group has been started
+                $conditionStarted = true;
             }
 
             // This is route end - call handler
@@ -43,8 +69,8 @@ class Core
                 $this->generateIf($data, $newPath, $code, $level + 5);
             }
 
+            // Close current route condition group
             $code .= $tabs . '}' . "\n";
-            $conditionStarted = true;
         }
 
         return $code;
