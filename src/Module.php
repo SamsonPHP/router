b6948c6faada8f655c68720114f374f0ca0fe2fd
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-01-13 14:53:01 +0200

    Moved locale parsing to initialization of module to give ability other modules to have it

--- src/Module.php
+++ src/Module.php
@@ -23,6 +23,9 @@ class Module extends \samson\core\CompressableExternalModule
     /** @var string Path to routing logic cache file */
     protected $cacheFile;
 
+    /** @var string Current URL path */
+    protected $requestURI;
+
     /**
      * Old generic "main_page" route callback searcher to match old logic.
      *
@@ -74,6 +77,11 @@ class Module extends \samson\core\CompressableExternalModule
 
         require($this->cacheFile);
         //[PHPCOMPRESSOR(remove,end)]
+
+        // This should be change to receive path as a parameter on initialization
+        $pathParts = explode(Route::DELIMITER, $_SERVER['REQUEST_URI']);
+        SamsonLocale::parseURL($pathParts);
+        $this->requestURI = implode(Route::DELIMITER, $pathParts);
 
         // Subscribe to samsonphp\core routing event
         \samsonphp\event\Event::subscribe('core.routing', array($this, 'router'));
@@ -143,7 +151,7 @@ class Module extends \samson\core\CompressableExternalModule
         // Flag for matching SamsonPHP asynchronous requests
         $async = $this->isAsynchronousRequest();
         // Get HTTP request path
-        $path = $_SERVER['REQUEST_URI'];
+        $path = $this->requestURI;//$_SERVER['REQUEST_URI'];
         // Get HTTP request method
         $method = $_SERVER['REQUEST_METHOD'];
         // Prepend HTTP request type, true - asynchronous
@@ -153,11 +161,6 @@ class Module extends \samson\core\CompressableExternalModule
 
         // Remove first slash if present, add method to path, remove GET params, remove trailing slash
         $path = rtrim(strtok($method.'/'.ltrim($path, '/'), '?'), '/');
-
-        // TODO: Optimize this
-        $pathParts = explode(Route::DELIMITER, $path);
-        SamsonLocale::parseURL($pathParts);
-        $path = implode(Route::DELIMITER, $pathParts);
 
         /** @var mixed $routeMetadata Dispatching result route metadata */
         if (is_array($routeMetadata = call_user_func(Core::ROUTING_LOGIC_FUNCTION, $path, $method))) {
