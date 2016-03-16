2c8af202419281e8d4b1a7fa5689c28dc61fc393
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-03-16 19:59:10 +0200

    Added found route checking

--- src/Module.php
+++ src/Module.php
@@ -168,53 +168,56 @@ class Module extends \samson\core\CompressableExternalModule
 
         /** @var mixed $routeMetadata Dispatching result route metadata */
         if (is_array($routeMetadata = call_user_func(Core::ROUTING_LOGIC_FUNCTION, $path, $method))) {
-            // Get callback info
-            list($module, $method) = explode("#", $routeMetadata[2]);
-            // Get module
-            $module = $core->module($module);
-            // Create callback
-            $callback = array($module, $method);
-
-            // Trigger found route event
-            Event::fire(self::EVENT_ROUTE_FOUND, array(&$module, $callback));
-
-            // Check if we have vaild callback
-            if (is_callable($callback)) {
-                // Get object from callback and set it as current active core module
-                $core->active($module);
-                // Routing result
-                $result = call_user_func_array(
-                    $callback,
-                    $this->parseParameters($callback, $routeMetadata[1])
-                );
-
-                // If this is cached method
-                if (stripos($method, self::CACHE_PREFIX) !== false) {
-                    // perform caching
-                    $core->cached();
-                }
-
-                // If this route is asynchronous
-                if ($async) {
-                    // Set async response
-                    $core->async(true);
-
-                    // If controller action has failed
-                    if (!isset($result['status']) || !$result['status']) {
-                        $result['message'] = "\n" . 'Event failed: ' . $routeMetadata[0];
-                        $result['status'] = 0;
+            // Check found route
+            if (count($routeMetadata[2]) === 2) {
+                // Get callback info
+                list($module, $method) = explode("#", $routeMetadata[2]);
+                // Get module
+                $module = $core->module($module);
+                // Create callback
+                $callback = array($module, $method);
+    
+                // Trigger found route event
+                Event::fire(self::EVENT_ROUTE_FOUND, array(&$module, $callback));
+    
+                // Check if we have vaild callback
+                if (is_callable($callback)) {
+                    // Get object from callback and set it as current active core module
+                    $core->active($module);
+                    // Routing result
+                    $result = call_user_func_array(
+                        $callback,
+                        $this->parseParameters($callback, $routeMetadata[1])
+                    );
+    
+                    // If this is cached method
+                    if (stripos($method, self::CACHE_PREFIX) !== false) {
+                        // perform caching
+                        $core->cached();
                     }
-
-                    // Encode event result as json object
-                    echo json_encode($result, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP);
-
-                    // Mark as successful
-                    $result = true;
+    
+                    // If this route is asynchronous
+                    if ($async) {
+                        // Set async response
+                        $core->async(true);
+    
+                        // If controller action has failed
+                        if (!isset($result['status']) || !$result['status']) {
+                            $result['message'] = "\n" . 'Event failed: ' . $routeMetadata[0];
+                            $result['status'] = 0;
+                        }
+    
+                        // Encode event result as json object
+                        echo json_encode($result, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP);
+    
+                        // Mark as successful
+                        $result = true;
+                    }
                 }
-            }
 
-            // If no result is passed - consider success
-            $result = $result !== false ? true : $result;
+                // If no result is passed - consider success
+                $result = $result !== false ? true : $result;
+            }
         }
 
         //elapsed('Finished routing');
